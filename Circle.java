14cfd0482972a5fc6214376c36c39eddba305103
mannymodrono <mmodrono21243@columbushs.com>
2020-09-30 09:21:44 -0400

    Started circle class

--- Circle.java
+++ Circle.java
@@ -1,9 +1,23 @@
 public class Circle {
     // instance vars
-
+    private double radius;
+    private final double pi = 3.14;
 
     //constructors
-
+    public Circle(double pRadius){
+        this.radius = pRadius;
+    }
 
     //methods
+    public void setRadius(double pRadius) {
+        this.radius = pRadius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return pi * radius * radius;
+    }
 }
