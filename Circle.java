8a00a30e61fb6b29d71513987d02f7791fa4cc9b
mannymodrono <mmodrono21243@columbushs.com>
2020-10-01 10:39:42 -0400

    Finished

--- Circle.java
+++ Circle.java
@@ -4,6 +4,10 @@ public class Circle {
     private final double pi = 3.14;
 
     //constructors
+    public Circle(){
+
+    }
+
     public Circle(double pRadius){
         this.radius = pRadius;
     }
@@ -19,5 +23,13 @@ public class Circle {
 
     public double getArea() {
         return pi * radius * radius;
+    }
+
+    public double getDiameter() {
+        return radius * 2.0;
+    }
+
+    public double getCircumference() {
+        return this.getDiameter() * pi;
     }
 }
--- CircleDemo.java
+++ CircleDemo.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class CircleDemo {
     public static void main(String[] args) {
-        Circle circle = new Circle();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Circle radius?");
+        Double radius = input.nextDouble();
+
+        Circle circle = new Circle(radius);
+
+        System.out.println("Circle's area is: " + circle.getArea());
+        System.out.println("Circle's diameter:  " + circle.getDiameter());
+        System.out.println("Circle's circumference: " + circle.getCircumference());
     }
 }
